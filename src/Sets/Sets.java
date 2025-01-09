fc51d2a971de1b8ed88ccee390c64c888694be32
PraveenMadavi <madavione@gmail.com>
2025-01-09 23:52:00 +0530

    TreeSet and LinkedHashSet

--- src/Sets/Sets.java
+++ src/Sets/Sets.java
@@ -42,5 +42,39 @@ public class Sets {
         //By using set : we can remove doublicate numbers
         Set<Integer> numberSet = new HashSet<>(numberList);
         System.out.println(numberSet);
+
+
+
+        Set<String> treeSet = new TreeSet<>();
+        treeSet.add("Akshay");
+        treeSet.add("xiochi");
+        treeSet.add("chai");
+        treeSet.add("fang");
+        treeSet.add("chai");//......X
+        treeSet.add("Akshay");//.....X
+        treeSet.add("lang");    // TreeSet<>() also doesn't add doublicate element
+
+        System.out.println(treeSet);
+
+        treeSet.forEach(System.out::println);
+        treeSet.remove("Akshay");
+        System.out.println("after removal of one element");
+        treeSet.forEach(System.out::println);
+
+
+        System.out.println("New LinkedHashSet<>()");
+        Set<String> names = new LinkedHashSet<>();
+        names.add("Sachin");
+        names.add("Toppa");
+        names.add("Vikas");
+        names.add("Rishikesh");
+        names.add("Purvi");
+        names.add("park");
+        names.add("Akshita");
+        names.add("Akshita"); //.......X
+
+        names.forEach(System.out::println);
+
+
     }
 }
