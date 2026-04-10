322cb580700c964688def22ac5d8ff1f4530d559
PraveenMadavi <madavione@gmail.com>
2026-04-11 01:20:23 +0530

    String Quest_stream.distinct

--- src/Java8/Streams/Main.java
+++ src/Java8/Streams/Main.java
@@ -7,6 +7,8 @@ import java.util.stream.Stream;
 public class Main {
     public static void main(String[] args) {
         Question question = new Question();
+
+        System.out.println(question.listOfNames);
         //Names in uppercase
 //        List<String> list = question.listOfNames.stream().map(String::toUpperCase).toList();
 //        System.out.println(list);
@@ -18,9 +20,12 @@ public class Main {
 //        //same but with different
 //        System.out.println(question.listOfNames.stream().filter(names -> names!=null && !names.isBlank()).map(names -> Character.toUpperCase(names.charAt(0))+names.substring(1)).toList());
 
-        Stream<String> streamedNames = question.listOfNames.stream();
-        Optional<String> streamedNamesFirst = streamedNames.findFirst();
-        streamedNamesFirst.ifPresent(System.out::println);
+//        Stream<String> streamedNames = question.listOfNames.stream();   // ONE TIME USE ONLY, NOT LIKE STORAGE MEDIA
+//        Optional<String> streamedNamesFirst = streamedNames.findFirst(); // Here 'streamedNames' is consumed so you can't use again.
+//        streamedNamesFirst.ifPresent(System.out::println);
+
+        List<String> distinctNames = question.listOfNames.stream().distinct().toList();
+        System.out.println(distinctNames);
 
 
     }
