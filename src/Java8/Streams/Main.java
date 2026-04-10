1d2dcb766fd2e9c9358fae7503f330ae40d39c05
PraveenMadavi <madavione@gmail.com>
2026-04-11 00:01:08 +0530

    String Ques-stream_findFirst

--- src/Java8/Streams/Main.java
+++ src/Java8/Streams/Main.java
@@ -1,6 +1,8 @@
 package Java8.Streams;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,10 +14,13 @@ public class Main {
 //        List<String> list1 = question.listOfNames.stream().map(names -> names.substring(0, 1).toUpperCase() + names.substring(1)).toList();
 //        System.out.println(list1);
         //if null-names
-        System.out.println(question.listOfNames.stream().filter(names -> names!=null && !names.isBlank()).map(names -> names.substring(0, 1).toUpperCase() + names.substring(1)).toList());
-        //same but with different
-        System.out.println(question.listOfNames.stream().filter(names -> names!=null && !names.isBlank()).map(names -> Character.toUpperCase(names.charAt(0))+names.substring(1)).toList());
+//        System.out.println(question.listOfNames.stream().filter(names -> names!=null && !names.isBlank()).map(names -> names.substring(0, 1).toUpperCase() + names.substring(1)).toList());
+//        //same but with different
+//        System.out.println(question.listOfNames.stream().filter(names -> names!=null && !names.isBlank()).map(names -> Character.toUpperCase(names.charAt(0))+names.substring(1)).toList());
 
+        Stream<String> streamedNames = question.listOfNames.stream();
+        Optional<String> streamedNamesFirst = streamedNames.findFirst();
+        streamedNamesFirst.ifPresent(System.out::println);
 
 
     }
