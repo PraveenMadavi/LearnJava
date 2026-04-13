018c08d889a3c9a002c2a62f089c8dab890591f8
PraveenMadavi <madavione@gmail.com>
2026-04-14 02:23:00 +0530

    Stream_secondBigNum

---
+++ src/Java8/Streams/Numbers.java
@@ -0,0 +1,22 @@
+package Java8.Streams;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class Numbers {
+
+    public static void main(String[] args) {
+//        List<Integer> integers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+        List<Integer> randomNums = Arrays.asList(45, 1, 2, 32, 5, 6, 89, 7, 67, 2, 1, 32, 23);
+
+        Integer secondBig = randomNums.stream()
+                .distinct()
+                .sorted(Comparator.reverseOrder())
+                .skip(2)
+                .findFirst()
+                .get();
+
+        System.out.println(secondBig);
+    }
+}
