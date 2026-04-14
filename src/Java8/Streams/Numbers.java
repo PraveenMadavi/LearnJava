1203798ecf1b2af9f356320241077913afeec0a4
PraveenMadavi <madavione@gmail.com>
2026-04-15 04:53:21 +0530

    stream-filter-duplicates

--- src/Java8/Streams/Numbers.java
+++ src/Java8/Streams/Numbers.java
@@ -1,31 +1,45 @@
 package Java8.Streams;
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
+
+import static java.util.stream.Collectors.toList;
 
 public class Numbers {
 
     public static void main(String[] args) {
         List<Integer> integers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
-//        List<Integer> randomNums = Arrays.asList(45, 1, 2, 32, 5, 6, 89, 7, 67, 2, 1, 32, 23);
+        List<Integer> randomNumbs = Arrays.asList(45, 1, 2, 32, 5, 6, 89, 7, 67, 2, 1, 32, 23, 5);
 
-//        Integer secondBig = randomNums.stream()
+//        Integer secondBig = randomNumbs.stream()
 //                .distinct()
 //                .sorted(Comparator.reverseOrder())
 //                .skip(2)
 //                .findFirst()
 //                .get();
-//
 //        System.out.println(secondBig);
 
 //        boolean allPositive = integers.stream().allMatch(n -> n > 0);
 //        System.out.println("All Positive : " + allPositive);
 
-        int sum = integers.stream().mapToInt(Integer::intValue)
-                .sum();
-        System.out.println(sum);
+        //STREAM - SUM
+//        int sum = integers.stream()
+//                .mapToInt(Integer::intValue)
+//                .sum(); //.average() for Avg
+//        System.out.println(sum);
 
+//        Collections.reverse(integers); // FOR REVERSE THE LIST OF INTEGERS
+//        int skippedSum = integers.stream()
+//                .mapToInt(Integer::intValue)
+//                .skip(4)
+//                .sum();
+//        System.out.println(skippedSum);
+
+        //FIND DUPLICATES
+        Set<Integer> numbs = new HashSet<>();
+        List<Integer> duplicates = randomNumbs.stream()
+                .filter(n -> !numbs.add(n))
+                .toList();
+        System.out.println("duplicates = " + duplicates);
 
     }
 }
