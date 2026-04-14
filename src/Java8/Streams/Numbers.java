3c6d172dd20f1339da4217077e17670a08aa366e
PraveenMadavi <madavione@gmail.com>
2026-04-14 23:59:51 +0530

    sum

--- src/Java8/Streams/Numbers.java
+++ src/Java8/Streams/Numbers.java
@@ -7,20 +7,25 @@ import java.util.List;
 public class Numbers {
 
     public static void main(String[] args) {
-        List<Integer> integers = Arrays.asList(1, 2, 3, 4, 5, 6, -7, 8, 9);
-        List<Integer> randomNums = Arrays.asList(45, 1, 2, 32, 5, 6, 89, 7, 67, 2, 1, 32, 23);
+        List<Integer> integers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+//        List<Integer> randomNums = Arrays.asList(45, 1, 2, 32, 5, 6, 89, 7, 67, 2, 1, 32, 23);
 
-        Integer secondBig = randomNums.stream()
-                .distinct()
-                .sorted(Comparator.reverseOrder())
-                .skip(2)
-                .findFirst()
-                .get();
+//        Integer secondBig = randomNums.stream()
+//                .distinct()
+//                .sorted(Comparator.reverseOrder())
+//                .skip(2)
+//                .findFirst()
+//                .get();
+//
+//        System.out.println(secondBig);
 
-        System.out.println(secondBig);
+//        boolean allPositive = integers.stream().allMatch(n -> n > 0);
+//        System.out.println("All Positive : " + allPositive);
 
-        boolean allPositive = integers.stream().allMatch(n -> n > 0);
-        System.out.println("All Positive : " + allPositive);
+        int sum = integers.stream().mapToInt(Integer::intValue)
+                .sum();
+        System.out.println(sum);
+
 
     }
 }
